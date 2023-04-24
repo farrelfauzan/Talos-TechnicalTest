7e91d1de215151104997efe97210d9eeb2068556
farrelfauzan <farrelfauzan78@gmail.com>
2023-04-24 15:52:08 +0700

    Done

---
+++ server/src/main.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+config();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  await app.listen(3000);
+  console.log(process.env.JWT_SECRET_KEY);
+}
+bootstrap();
